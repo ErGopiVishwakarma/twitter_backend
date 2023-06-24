2b485e6f80be7d2f68bfd2c264d516336fd6e94b
Gopi vishwakarma <g2468vi@gmail.com>
2023-06-24 15:48:11 +0530

    code added

--- Route/auth.js
+++ Route/auth.js
@@ -2,8 +2,8 @@ const authRouter = require("express").Router();
 const passport = require("passport");
 const jwt = require('jsonwebtoken')
 const UserModel = require('../model/userModel')
-const successRedirect = `https://twitter-hdsfx1kvl-ergopivishwakarma.vercel.app/`
-const failedRedirect = `https://twitter-hdsfx1kvl-ergopivishwakarma.vercel.app/auth`
+const successRedirect = `https://twitter-orpin-sigma.vercel.app/`
+const failedRedirect = `https://twitter-orpin-sigma.vercel.app/auth`
 
 authRouter.get('/', (req, res) => {
 	res.send('this is auth page')
